d9463e3c92d9d0ec6dbd384002d27b0a340d3e54
Saba <mariogabriele02.sabbatini@gmail.com>
2023-07-13 17:52:12 +0300

    Restaurant screen

--- athens/lib/screens/home/home.dart
+++ athens/lib/screens/home/home.dart
@@ -2,7 +2,16 @@ import 'package:athens/constants/theme_model.dart';
 import 'package:athens/screens/food/food_list.dart';
 import 'package:athens/screens/restaurants/restaurants_list.dart';
 import 'package:flutter/material.dart';
+<<<<<<< Updated upstream:athens/lib/screens/home/home.dart
 import 'home_helpers.dart/profile_card.dart';
+=======
+
+import '../constants/theme_model.dart';
+import 'home_helpers.dart/invite_friend.dart';
+import 'home_helpers.dart/profile_card.dart';
+import 'restaurants/restaurants_list.dart';
+import 'trivia/daily_trivia_card.dart';
+>>>>>>> Stashed changes:athens/lib/screens/home.dart
 
 class Home extends StatefulWidget {
   @override
@@ -12,7 +21,7 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  final containerRadius = BorderRadius.all(Radius.circular(25));
+  final containerRadius = BorderRadius.all(Radius.circular(20));
   final ThemeModel theme = ThemeModel.instance;
 
   @override
@@ -25,6 +34,7 @@ class _HomeState extends State<Home> {
           Container(
             padding: EdgeInsets.only(left: 30, top: 10),
             child: Text(
+<<<<<<< Updated upstream:athens/lib/screens/home/home.dart
               'Restaurants suggested for you',
               style: TextStyle(
                 fontSize: 20,
@@ -39,6 +49,8 @@ class _HomeState extends State<Home> {
           Container(
             padding: EdgeInsets.only(left: 30, top: 10),
             child: Text(
+=======
+>>>>>>> Stashed changes:athens/lib/screens/home.dart
               'Best value boxes',
               style: TextStyle(
                 fontSize: 20,
@@ -50,9 +62,23 @@ class _HomeState extends State<Home> {
             height: 240,
             child: FoodList(),
           ),
-          //DailyTrivia(),
-          //DailyChallenge(),
-          //InviteFriendsCard(),
+          DailyTrivia(),
+          Container(
+            padding: EdgeInsets.only(left: 30, top: 10),
+            child: Text(
+              'Restourants suggested for you',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+            height: 240,
+            child: RestaurantsList(),
+          ),
+          InviteFriendsCard(),
         ],
       ),
     );
