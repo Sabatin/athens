4f5dfc7c795e3d9183dbd297fc3e484a29ba3e11
Aginius <francesco.barabino@tiscali.it>
2023-07-13 14:51:35 +0300

    f

--- athens/lib/screens/restaurant/restaurant_screen.dart
+++ athens/lib/screens/restaurant/restaurant_screen.dart
@@ -1,6 +1,8 @@
 import 'package:athens/model/food.dart';
 import 'package:athens/model/restaurant.dart';
+import 'package:athens/screens/food/food_card.dart';
 import 'package:athens/screens/restaurant/restaurant_image.dart';
+import 'package:athens/screens/utils/loading_indicator.dart';
 import 'package:athens/service/food_service.dart';
 import 'package:flutter/material.dart';
 
@@ -27,9 +29,20 @@ class RestaurantScreen extends StatelessWidget {
               future: FoodService().getFood(restaurant.id),
               builder: (context, food) {
                 if (food.hasData) {
-
+                  return ListView.separated(
+                    itemCount: food.requireData.length,
+                    itemBuilder: (context, index) {
+                      return FoodCard(food.requireData[index]);
+                    },
+                    separatorBuilder: (context, index) {
+                      return SizedBox(height: 20);
+                    },
+                  );
                 }
-                return SizedBox();
+                else if (food.hasError) {
+                  return Text("Error");
+                }
+                return LoadingIndicator();
               },
             )
           ],
