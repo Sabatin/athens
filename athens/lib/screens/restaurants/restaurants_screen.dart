cd3c87a0e6b4c4980e00452abd2a5c877dcf1ddd
Aginius <francesco.barabino@tiscali.it>
2023-07-13 13:36:47 +0300

    f

--- athens/lib/screens/restaurants/restaurants_screen.dart
+++ athens/lib/screens/restaurants/restaurants_screen.dart
@@ -13,19 +13,30 @@ class RestaurantsScreen extends StatelessWidget {
         child: Column(
           children: [
             AppBar(title: Text("ye")),
-            FutureBuilder<List<Restaurant>>(
-              future: RestaurantsService().getRestaurants(),
-              builder: (context, restaurants) {
-                return ListView.separated(
-                  itemCount: restaurants.requireData.length,
-                  itemBuilder: (context, index) {
-                    return RestaurantCard(restaurants.requireData[index]);
-                  },
-                  separatorBuilder: (context, index) {
-                    return SizedBox(height: 30);
-                  },
-                );
-              }
+            Expanded(
+              child: FutureBuilder<List<Restaurant>>(
+                future: RestaurantsService().getRestaurants(),
+                builder: (context, restaurants) {
+                  if (restaurants.hasData) {
+                    return ListView.separated(
+                      itemCount: restaurants.requireData.length,
+                      itemBuilder: (context, index) {
+                        return RestaurantCard(restaurants.requireData[index]);
+                      },
+                      separatorBuilder: (context, index) {
+                        return SizedBox(height: 30);
+                      },
+                    );
+                  }
+                  else if (restaurants.hasError) {
+                    return Text("Error");
+                  }
+                  return SizedBox(
+                    height: 20,
+                    width: 20,
+                    child: CircularProgressIndicator());
+                }
+              ),
             ),
           ],
         )
